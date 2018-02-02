3b4c4867e860bcd7f980b05feeef06ca60536630
Hiroyuki Okamura <okamu@hiroshima-u.ac.jp>
2018-02-03 01:58:32 +0900

    add poissontest

--- test/test_poisson.cpp
+++ test/test_poisson.cpp
@@ -2,65 +2,68 @@
   test
 */
 
+#include <iostream>
+#include <iomanip>
+#include <random>
+
 #include "marlib.h"
 
+#include "vector.h"
+
 using namespace marlib;
+
+std::random_device seed_gen;
+std::mt19937 engine(seed_gen());
+std::uniform_real_distribution<> dist(0, 10000.0);
+
+std::string check_equal(const std::string& title, int n, const double *x, int incx, const double *y, int incy) {
+  std::cout << title << ": ";
+  for (int i=0; i<n; i++, x+=incx, y+=incy) {
+    if (std::abs(*x - *y) > 1.0e-10) {
+      std::cout << "false: " << i << " " << (*x - *y) << std::endl;
+      return "false";
+    }
+  }
+  return "true";
+}
+
+void random_vector(int n, double* x) {
+  for (int i=0; i<n; i++, x++) {
+    *x = dist(engine);
+    if (*x < 0) {
+      *x = 0;
+    }
+  }
+}
+
+template <typename T>
+void print(const T& x) {
+#ifdef DEBUG
+  std::cout << x << std::endl;
+#endif
+}
 
 void test_poi1() {
   std::cout << "poi1" << std::endl;
 
-  poisson<double,int> poi(1.0, 1.0e-8);
+  double lambda = dist(engine);
+  int right = poisson<double>::rightbound(lambda, 1.0e-8);
+  double prob[right+1];
+  vector vprob(right+1, prob);
+  poisson<double> poi(right+1, prob);
 
-  std::cout << poi.left() << std::endl;
-  std::cout << poi.right() << std::endl;
-  std::cout << poi.get_prob() << std::endl;
+  poi.set(lambda, 0, right);
+  print(vprob);
 
-  vector<double,int> x(poi.right() - poi.left() + 1, poi.get_prob(), 1);
-  x.set_range(range<int>(poi.left(),poi.right()));
-  x /= poi.weight();
-  std::cout << x << std::endl;
-
-  std::cout << poi.get_prob() << std::endl;
-}
-
-void test_poi2() {
-  std::cout << "poi2" << std::endl;
-
-  poisson<double,int> poi(1.0, 1.0e-8);
-
-  std::cout << poi.left() << std::endl;
-  std::cout << poi.right() << std::endl;
-  std::cout << poi.get_prob() << std::endl;
-
+  double sum = 0;
+  double one = 1;
   for (int i=poi.left(); i<=poi.right(); i++) {
-    std::cout << poi(i) << std::endl;
+    sum += poi(i);
   }
-
-  poi.set(0.8, 1.0e-8);
-
-  std::cout << "----------" << std::endl;
-
-  for (int i=poi.left(); i<=poi.right(); i++) {
-    std::cout << poi(i) << std::endl;
-  }
-}
-
-void test_poi3() {
-  std::cout << "poi3" << std::endl;
-
-  poisson<double,int> poi(0.0, 1.0e-8);
-
-  std::cout << poi.left() << std::endl;
-  std::cout << poi.right() << std::endl;
-  std::cout << poi.get_prob() << std::endl;
-
-  for (int i=poi.left(); i<=poi.right(); i++) {
-    std::cout << poi(i) << std::endl;
-  }
+  sum /= poi.weight();
+  std::cout << check_equal("test poi", 1, &sum, 1, &one, 1) << std::endl;
 }
 
 int main() {
   test_poi1();
-  test_poi2();
-  test_poi3();
 }
